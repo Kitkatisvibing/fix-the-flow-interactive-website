d4e5abfcc0c30d090b0fdd8d1278650f5bd89680
Kitkatisvibing <kate.jadi@gmail.com>
2025-12-12 11:05:53 +0100

    menu verduidelijking #8

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,12 @@
 // 1. Selecteer de button waarmee ik mijn menu open
-let Button = document.querySelector('.menuButton')
+let menuButton = document.querySelector('.menuButton')
 // 2. Wacht tot iemand erop klikt
-Button.addEventListener('click',function() {
+menuButton.addEventListener('click',function() {
 // 3. Verander de class van mijn <nav>, zodat deze zichtbaar wordt
 document.querySelector('nav').classList.toggle('menuOpen')
+document.querySelector('.menuButton').classList.toggle('open')
 })
+
+//1. selecteer het element dat moet bewegen
+//2. voeg de click eventlistener toe
+//3. toggle de class
